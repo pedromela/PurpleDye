2498e4c6eaeec5ff55a73ef424874399bb4de34a
pedro miguel <pedro.miguel.69@hotmail.com>
2020-11-14 13:25:22 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -18,8 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['pytest'],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
     python_requires='>=3.8'
 )
